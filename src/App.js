84dfc8bcb0f1d7b7558f90fd641352007e779449
Lucas <lucasmachado010207@gmail.com>
2024-07-16 17:25:25 -0300

    toggletheme

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { ThemeProvider } from './components/ThemeContext';
+import Theme from './components/Theme';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider>
+        <Theme />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
